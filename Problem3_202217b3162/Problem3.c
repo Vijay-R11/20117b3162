5340e301e744639cf1ab182c5ee1a0d4653dc528
Vijay R <159248622+Vijay-R11@users.noreply.github.com>
2024-07-21 15:23:16 +0000

    Add files via upload

---
+++ Problem3_202217b3162/Problem3.c
@@ -0,0 +1,92 @@
+#include<stdio.h>
+
+// Global variables
+int stack[100];  // Stack array
+int top = -1;    // Initialize top of stack
+int n;           // Size of the stack
+int choice, x, i; // User choice, value to be pushed, and index variable
+
+// Function prototypes
+void push(void);
+void pop(void);
+void display(void);
+
+int main()
+{
+    printf("\n Enter the size of STACK[MAX=100]:");
+    scanf("%d", &n);
+
+    printf("\n\t STACK OPERATIONS USING ARRAY");
+    printf("\n\t--------------------------------");
+    printf("\n\t 1.PUSH\n\t 2.POP\n\t 3.DISPLAY\n\t 4.EXIT");
+
+    do
+    {
+        printf("\n Enter the Choice:");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+            case 1:
+                push();
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                printf("\n\t EXIT POINT\n");
+                break;
+            default:
+                printf("\n\t Please Enter a Valid Choice (1/2/3/4)");
+        }
+    } while (choice != 4);
+
+    return 0;
+}
+
+void push()
+{
+    if (top >= n - 1)
+    {
+        printf("\n\tSTACK is overflow");
+    }
+    else
+    {
+        printf(" Enter a value to be pushed:");
+        scanf("%d", &x);
+        top++;
+        stack[top] = x;
+    }
+}
+
+void pop()
+{
+    if (top <= -1)
+    {
+        printf("\n\t Stack is underflow");
+    }
+    else
+    {
+        printf("\n\t The popped element is %d", stack[top]);
+        top--;
+    }
+}
+
+void display()
+{
+    if (top >= 0)
+    {
+        printf("\n The elements in STACK \n");
+        for (i = top; i >= 0; i--)
+        {
+            printf("%d\n", stack[i]);
+        }
+    }
+    else
+    {
+        printf("\n The STACK is empty");
+    }
+}
